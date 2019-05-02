32f04ee2b65e2dd63e486769e542dfeefa4e2c00
Dmitriy Braginets <dmitriy.braginets@qbex.io>
2019-05-02 12:36:29 +0300

    add full promise functionality (then, catch, chaining)

--- Parole.js
+++ Parole.js
@@ -1,34 +1,71 @@
-const C = require('./lib/constants');
+/**
+ * Object of constants to describe Parole state
+ * 
+ * @type {Object}
+ */
+const C = {
+    PENDING: 'Pending',
+    RESOLVED: 'Resolved',
+    REJECTED: 'Rejected'
+}
 
+/**
+ * Stub function to implement native behaviour of promise
+ * 
+ * @param {*} value 
+ */
 function defaultFn(value){
     return value
 }
 
+/**
+ * "Class" Parole with basic Promise functionality
+ * 
+ * @param {*} initialCallback 
+ */
 function Parole(initialCallback) {
 
-    var _deferedCallbacks = [];
+    /**
+     * @type {Array}
+     */
+    var _deferredCallbacks = [];
 
-    // function callLater(chainFunction){
-    //     return function(callback){
-    //         return new Parole(function(resolve){
-    //             _deferedCallbacks.push(resolve(chainFunction()(callback)))
-    //         })
-    //     }
-    // }
-
-    // function callLater(chainFunction){
-    //     console.log('chainFunction', chainFunction)
-    //     return 
-    // }
-
-    const callLater = getMember => {
-        return callback => {
-            return new Parole(resolve => {
-                _deferedCallbacks.push(() => resolve(getMember()(callback)))
-            });
+    /**
+     * Push deferred callbacks in stack to call them after promise resolving
+     * Such nesting is necessary to catch permanent state of "then" method and Parole own state
+     * 
+     * @param {Function} chainFunction 
+     */
+    function callLater(chainFunction){
+        return function(callback){
+            return new Parole(function(resolve){
+                _deferredCallbacks.push(function(){
+                    resolve(chainFunction()(callback))
+                })
+            })
         }
     }
-    
+
+    /**
+     * Closure for then method
+     */
+    function _thenLater(){
+        return this.then
+    }
+
+    /**
+     * Closure for cathc method
+     */
+    function _catchLater(){
+        return this.catch
+    }
+
+
+    /**
+     * Implements behaviour of proposal https://github.com/tc39/proposal-promise-try/blob/master/polyfill.js#L12
+     * 
+     * @param {Function} callback 
+     */  
     function _tryCall(callback){
         function innerCallback(){
             return callback(this._result)
@@ -36,8 +73,16 @@ function Parole(initialCallback) {
         return Parole.try(innerCallback.bind(this))
     }
 
+    /**
+     * @type {Function}
+     */
     var tryCall = _tryCall.bind(this)
-  
+    
+    /**
+     * Change state of Parole instance
+     *  
+     * @param {string} state 
+     */
     function _changeState(state){
         var newState = {}
 
@@ -59,10 +104,8 @@ function Parole(initialCallback) {
             default:
                 newState = {
                     _state: C.PENDING,
-                    then: callLater(() => {
-                        return this.then}
-                        ),
-                    catch: callLater(() => this.catch)
+                    then: callLater(_thenLater.bind(this)),
+                    catch: callLater(_catchLater.bind(this))
                 }
                 break;
         }
@@ -70,31 +113,43 @@ function Parole(initialCallback) {
         Object.assign(this, newState);
     }
 
+    /**
+     * Closure for _changeState function
+     * 
+     * @type {Function}
+     */
     var changeState = _changeState.bind(this)
 
+    /**
+     * Apply state and value of resolved promise
+     * 
+     * @param {string} state 
+     * @param {*} value 
+     */
     function _apply(state, value){
         if (this._state === C.PENDING){
             this._result = value;
-            // console.log('PAROLE APLLY', this._result)
             changeState(state)
-            // console.log('PAROLE APLLY CHANGE STATE', this)
-            // console.log('PAROLE _deferedCallbacks', _deferedCallbacks)
-            _deferedCallbacks.forEach(function(cb){
-                if (cb){
-                    console.dir(cb)
-                    cb()
+
+            _deferredCallbacks.forEach(function(deferredCallback){
+                if (deferredCallback){
+                    deferredCallback()
                 }
             })
         }
     }
     
-
+    /**
+     * Closure that initialize resolve and reject methods
+     * 
+     * @param {string} state
+     * 
+     * @returns {Function}
+     */
     function _makePromiseCallback(state){
         return function(value){
-            // console.log('PAROLE  _makePromiseCallback', this, state, value)
             var apply = _apply.bind(this)
             if (value instanceof Parole && state === C.RESOLVED){
-                // console.log('PAROLE _makePromiseCallback inner', this, state, value)
                 value.then(function(value){
                     apply(C.RESOLVED, value)
                 })
@@ -104,17 +159,35 @@ function Parole(initialCallback) {
             }else if(value){
                 apply(state, value)
             }
-            // console.log('_makePromiseCallback after', this, state, value)
          }
     }
    
-
+    /**
+     * Closure 
+     * 
+     * @type {Function}
+     */
     var _resolve = _makePromiseCallback(C.RESOLVED);
 
+    /**
+     * Closure 
+     * 
+     * @type {Function}
+     */
     var _reject = _makePromiseCallback(C.REJECTED);
 
+    /**
+     * Closure 
+     * 
+     * @type {Function}
+     */
     var resolve = _resolve.bind(this)
 
+    /**
+     * Closure 
+     * 
+     * @type {Function}
+     */
     var reject = _reject.bind(this)
 
     changeState(C.PENDING);
@@ -126,19 +199,27 @@ function Parole(initialCallback) {
     }
 }
 
-
+/**
+ * "Static" Parole method
+ */
 Parole.resolve = function(value) {
     return new Parole(function(resolve){
         resolve(value)
     })
 }
 
+/**
+ * "Static" Parole method
+ */
 Parole.reject = function(value) {
     return new Parole(function(undefined, reject){
         reject(value)
     })
 }
 
+/**
+ * "Static" Parole method
+ */
 Parole.try = function(callback){
     return new Parole(function(resolve){
         resolve(callback())
--- test.js
+++ test.js
@@ -1,54 +1,13 @@
 
-const PromiseCustom = require('./PromiseCustom');
+const Parole = require('./Parole');
 
-
-const testSecond = new PromiseCustom((resolve, reject) => {
+const test = new Parole((resolve)=>{
     setTimeout(resolve, 1000, 1)
-})
-
-
-testSecond.then(result => {
-    console.log('IN UPDATED THEN RESULT', result);
-    // return 2
-    return new PromiseCustom((resolve, reject)=>{
-        setTimeout(resolve, 2000, '!!!!!!!')
-    });
 }).then(result =>{
-    console.log('IN UPDATED THEN FIRST CHAIN RESULT',result)
-}).catch(error => {
-    console.error('IN UPDATED CATCH', error)
-})
-
-testSecond.then(result => {
-    console.log('IN UPDATED SECOND THEN RESULT', result)
-})
-
-testSecond.catch(result => {
-    console.error('IN UPDATED SECOND CATCH RESULT', result)
-})
-
-
-const testNative = new Promise((resolve, reject) => {
-    setTimeout(resolve, 1000, 1)
-})
-
-
-testNative.then(result => {
-    console.log('IN UPDATED THEN RESULT', result);
-    // return 2
-    return new Promise((resolve, reject)=>{
-        setTimeout(resolve, 2000, '!!!!!!!')
-    });
-}).then(result =>{
-    console.log('IN UPDATED THEN FIRST CHAIN RESULT',result)
-}).catch(error => {
-    console.error('IN UPDATED CATCH', error)
-})
-
-testNative.then(result => {
-    console.log('IN UPDATED SECOND THEN RESULT', result)
-})
-
-testNative.catch(result => {
-    console.error('IN UPDATED SECOND CATCH RESULT', result)
+    console.log('THEN', result)
+    return new Parole((resolve)=>{
+        setTimeout(resolve, 1000, 1 + result)
+    })
+}).then(result => {
+    console.log('THIRD', result)
 })
